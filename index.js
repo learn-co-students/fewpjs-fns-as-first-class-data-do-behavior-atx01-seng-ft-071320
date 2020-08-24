027d5bea487c6000c29bdf040336d93d669e2c62
allenorton <norton.allene@gmail.com>
2020-08-24 15:28:33 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  const timeArray = time.split(":");
+  const hour = timeArray[0];
+  //const minute = timeArray[1]
+  if (hour < 12) {
+    return 'Good Morning';
+  } else if (hour >= 12 && hour < 18) {
+    return 'Good Afternoon';
+  } else {
+    return 'Good Evening';
+  };
+}
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
